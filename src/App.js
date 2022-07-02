5041eea127d5e2bdfab04b7930d3aebcf847f0fd
Ted Fitzpatrick <tedfitzpatrick@me.com>
2022-07-02 13:46:51 -0700

    adding nav links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import logo from './logo.svg';
 import {
   BrowserRouter as Router,
+  Routes,
   Route,
-  Routes
+  Link
 } from 'react-router-dom';
+
+import Nav from './Nav';
 
 import Home from './screens/Home/Home';
 import About from './screens/About/About';
@@ -16,13 +19,16 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <Nav />
+      <div>
       <Routes>
-    <Route path="/" exact component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/career" component={Career} />
-    <Route path="/education" component={Education} />
-    <Route path="/portfolio" component={Portfolio} />
-    </Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/career" element={<Career />} />
+      <Route path="/education" element={<Education />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+      </div>
     </Router>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+
+    <nav>
+        
+        <ul>
+
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/career'>Career</Link></li>
+            <li><Link to='/education'>Education</Link></li>
+            <li><Link to='/portfolio'>Portfolio</Link></li>
+            <li><Link to='/about'>About</Link></li>
+
+        </ul>
+
+    </nav>
+
+};
+
+export default Nav;
